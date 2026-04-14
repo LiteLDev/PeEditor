9d624e2a1606f44078f8fa113935aaf179deaaff
ShrBox <shrbox@outlook.com>
2026-04-14 19:05:55 +0800

    chore(deps): bump workflow versions chore: optimize xmake.lua optimize tooth.json add dependabot.yml

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,6 @@
 add_rules("mode.debug", "mode.release")
 
-add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
+add_repositories("levimc-repo https://github.com/LiteLDev/xmake-repo.git")
 
 add_requires("cxxopts")
 add_requires("fmt")
@@ -10,8 +10,6 @@ add_requires("pe_bliss")
 
 set_runtimes("MD")
 
-local version = "v3.9.0"
-
 target("PeEditor")
     set_kind("binary")
     set_languages("c++20")
@@ -20,8 +18,17 @@ target("PeEditor")
     add_includedirs("src")
     add_cxflags("/utf-8", "/EHa")
     set_exceptions("none")
-    add_defines("UNICODE", "PE_EDITOR_VERSION=\"" .. version .. "\"", "_CRT_NONSTDC_NO_DEPRECATE")
+    add_defines("UNICODE", "_CRT_NONSTDC_NO_DEPRECATE")
     add_syslinks("wsock32", "comdlg32")
     add_packages(
         "cxxopts", "fmt", "spdlog", "pe_bliss"
     )
+    on_load(function(target)
+        local tag = os.iorun("git describe --tags --abbrev=0 --always")
+        local major, minor, patch, suffix = tag:match("v(%d+)%.(%d+)%.(%d+)(.*)")
+        if not major then
+            print("Failed to parse version tag, using 0.0.0")
+            major, minor, patch = 0, 0, 0
+        end
+        target:add("defines", "PE_EDITOR_VERSION=\"" .. major .. "." .. minor .. "." .. patch .. "\"")
+    end)
